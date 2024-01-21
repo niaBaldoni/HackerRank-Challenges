229febaef911977bfe3598650bb64a8ddfd8c887
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-21 22:52:15 +0100

    code for calendar-module.py

--- python/calendar-module.py
+++ python/calendar-module.py
@@ -1,1 +1,10 @@
-# temp
+import calendar
+
+# we get the input in a single line, containing the space separated month, day and year
+# date = [mm, dd, yyyy]
+date = list(map(int, input().split()))
+dd = date[1]
+mm = date[0]
+yyyy = date[2]
+
+print((calendar.day_name)[calendar.weekday(yyyy, mm, dd)].upper())
