43824d164f6b522de4ca6d189c469be648301406
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-22 00:05:05 +0100

    Update exceptions.py

--- python/exceptions.py
+++ python/exceptions.py
@@ -1,1 +1,8 @@
-# temp
+for i in range(int(input())):
+    case = input().split()
+    try:
+        res = int(case[0])//int(case[1])
+    except Exception as e:
+        print("Error Code:", e)
+    else:
+        print(res)
