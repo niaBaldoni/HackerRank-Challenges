10d1e1cdce04fd390026f14b4bceca7d291948a5
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-22 00:10:05 +0100

    Update py-check-strict-superset.py

--- python/py-check-strict-superset.py
+++ python/py-check-strict-superset.py
@@ -1,1 +1,13 @@
-# temp
+setA = set(map(int, input().split()))
+
+check = True
+
+for i in range(int(input())):
+
+    setB = set(map(int, input().split()))
+
+    if not (setA.difference(setB) and not setB.difference(setA)):
+
+        check = False
+
+print(check)
