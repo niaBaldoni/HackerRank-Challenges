8f98912e0e3ab50ec77aff5ece218a7b947c9388
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-22 00:04:31 +0100

    Update defaultdict-tutorial.py

--- python/defaultdict-tutorial.py
+++ python/defaultdict-tutorial.py
@@ -1,1 +1,17 @@
-# temp
+from collections import defaultdict
+
+n, m = input().split()
+a = defaultdict(list)
+lista = []
+
+for i in range(int(n)):
+    temp = input()
+    lista.append(temp)
+    a[temp].append(len(lista))
+
+for i in range(int(m)):
+    temp = input()
+    if temp in a:
+        print(*a[temp])
+    else:
+        print(-1)
