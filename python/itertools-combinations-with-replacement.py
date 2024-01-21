58875e1188332842a266d8abbf4d037b54ab64ef
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-22 00:07:37 +0100

    Update itertools-combinations-with-replacement.py

--- python/itertools-combinations-with-replacement.py
+++ python/itertools-combinations-with-replacement.py
@@ -1,1 +1,7 @@
-# temp
+from itertools import combinations_with_replacement
+
+s, k = input().split()
+ 
+out = ["".join(i) for i in combinations_with_replacement(sorted(s), int(k))]
+
+print(*out, sep="\n")
