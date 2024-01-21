9522c57744fd1ffd072a898699ec0a900773cd1f
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-22 00:11:52 +0100

    Update py-collections-namedtuple.py

--- python/py-collections-namedtuple.py
+++ python/py-collections-namedtuple.py
@@ -1,1 +1,12 @@
-# temp
+from collections import namedtuple
+
+n = int(input())
+Student = namedtuple("Student", input().split())
+total = 0
+
+for i in range(n):
+    f1, f2, f3, f4 = input().split()
+    temp = Student(f1, f2, f3, f4)
+    total += int(temp.MARKS)
+
+print(round(total/n, 2))
