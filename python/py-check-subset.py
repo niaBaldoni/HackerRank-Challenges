e8be624fcb1026f5b6d4cd23eb2a7067a5efdb04
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-22 00:11:22 +0100

    Update py-check-subset.py

--- python/py-check-subset.py
+++ python/py-check-subset.py
@@ -1,1 +1,10 @@
-# temp
+
+tests = int(input())
+
+for i in range(tests):
+    input()
+    setA = set(map(int, input().split()))
+    input()
+    setB = set(map(int, input().split()))
+
+    print(not bool(setA.difference(setB)))
