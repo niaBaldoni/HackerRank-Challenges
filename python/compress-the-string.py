7dcc5227deb89c405fcd933b24ea31fc82a28f12
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-22 00:04:11 +0100

    added code for compress-the-string.py

--- python/compress-the-string.py
+++ python/compress-the-string.py
@@ -1,1 +1,15 @@
-# temp
+from itertools import groupby
+
+L = input()
+
+"""
+ris = ""
+lista = []
+for key, group in groupby(L):
+    ris += f"({len(list(group))}, {key}) "
+print(ris)
+
+"""
+
+out = [(len(list(group)), int(key)) for key, group in groupby(L)]
+print(*out, sep=" ")
