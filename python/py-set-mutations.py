599de8d3c969d6c235be33ab33353161a9ef93f7
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-22 00:14:59 +0100

    Update py-set-mutations.py

--- python/py-set-mutations.py
+++ python/py-set-mutations.py
@@ -1,1 +1,7 @@
-# temp
+input()
+L = set(input().split())
+for _ in range(int(input())):
+    command, *args = input().split()
+    getattr(L, command)(set(input().split()))
+
+print(sum(map(int, L)))
