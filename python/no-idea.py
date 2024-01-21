09f27e74a424023858cec63ec57a57b99a58b68a
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-22 00:09:13 +0100

    Update no-idea.py

--- python/no-idea.py
+++ python/no-idea.py
@@ -1,1 +1,6 @@
-# temp
+n, m = input().split()
+arr = [int(x) for x in input().split()]
+A = set([int(x) for x in input().split()])
+B = set([int(x) for x in input().split()])
+
+print(sum([(i in A) - (i in B) for i in arr]))
