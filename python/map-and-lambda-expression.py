6ed3ab95ea1e470e9fe89271d8bc045b8f0a788a
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-22 00:08:40 +0100

    Update map-and-lambda-expression.py

--- python/map-and-lambda-expression.py
+++ python/map-and-lambda-expression.py
@@ -1,1 +1,11 @@
-# temp
+cube = lambda x: x**3
+
+def fibonacci(n):
+    lista = [0, 1]
+    for i in range(2, n):
+        lista.append(lista[i-2] + lista[i-1])
+    return lista[0:n]
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
