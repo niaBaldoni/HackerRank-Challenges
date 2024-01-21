a0bec82a9d6846f9d41edeb808739a0561312781
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-22 00:18:56 +0100

    Update symmetric-difference.py

--- python/symmetric-difference.py
+++ python/symmetric-difference.py
@@ -1,1 +1,8 @@
-# temp
+
+m = int(input())
+listaM = list(map(int, input().split()))
+
+n = int(input())
+listaN = list(map(int, input().split()))
+
+print("\n".join(map(str, sorted([x for x in set(listaM).union(set(listaN)) - set(listaM).intersection(set(listaN))]))))
