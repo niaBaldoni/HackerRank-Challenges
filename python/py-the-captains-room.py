c5a7d26bbefabe3e033186d770e781e5131e7147
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-22 00:17:18 +0100

    Update py-the-captains-room.py

--- python/py-the-captains-room.py
+++ python/py-the-captains-room.py
@@ -1,1 +1,27 @@
-# temp
+'''
+k = int(input())
+lista = list(map(int, input().split()))
+
+counts = dict()
+for el in lista:
+    if el not in counts:
+        counts[el] = 1
+    else:
+        counts[el] += 1
+
+x = min(counts, key=counts.get)
+
+print(x)
+'''
+
+k = int(input())
+lista = list(map(int, input().split()))
+
+setA, setB = set(), set()
+for i in lista:
+    if i not in setA:
+        setA.add(i)
+    else:
+        setB.add(i)
+
+print(*setA.difference(setB))
