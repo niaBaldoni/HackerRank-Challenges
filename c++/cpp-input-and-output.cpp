1ca85e9dd44517244bfd29063813135a99629b1d
niaBaldoni <eugenia.v.baldoni@gmail.com>
2024-01-22 10:44:55 +0100

    added comments

--- c++/cpp-input-and-output.cpp
+++ c++/cpp-input-and-output.cpp
@@ -7,10 +7,13 @@ using namespace std;
 
 
 int main() {
+    // declare three int variables to store three numbers
     int n1, n2, n3;
-    
+
+    // read three int from input stream
     cin >> n1 >> n2 >> n3;
-    
+
+    // execute sum and print it
     cout << n1 + n2 + n3 << endl;
     return 0;
 }
