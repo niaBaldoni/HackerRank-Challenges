90f761845e5b8c915470f1099522d71a8e5eb4ba
nia <16371386+niaBaldoni@users.noreply.github.com>
2024-03-04 15:45:41 +0100

    fixed encoding 5/?

--- python/exceptions.py
+++ python/exceptions.py
@@ -1,8 +1,8 @@
-for i in range(int(input())):
-    case = input().split()
-    try:
-        res = int(case[0])//int(case[1])
-    except Exception as e:
-        print("Error Code:", e)
-    else:
-        print(res)
+for i in range(int(input())):
+    case = input().split()
+    try:
+        res = int(case[0])//int(case[1])
+    except Exception as e:
+        print("Error Code:", e)
+    else:
+        print(res)
